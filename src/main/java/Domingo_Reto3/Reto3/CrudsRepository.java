0ef8bcacb35590fd14a63a44ab88e71bb8d0642f
DidierMV <myemail>
2021-11-02 01:45:46 -0500

    first commit

---
+++ src/main/java/Domingo_Reto3/Reto3/CrudsRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Domingo_Reto3.Reto3;
+
+/**
+ *
+ * @author magom
+ */
+interface CrudsRepository<T0, T1> {
+    
+}
